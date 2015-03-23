4edc46cea08c787850213a58e32276e6ecb2fd9a
Luke Merry <Luke@MacBook-Pro.local>
2015-03-22 21:51:16 -0600

    tutorial works!

---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<html>
+        <head>
+                <title>CodeIgniter Tutorial</title>
+        </head>
+        <body>
+
+                <h1>CodeIgniter Tutorial</h1>
+                <br />
+                <br />
